0c92d2d05e6e5a2db119c226c65390a2ddd10f50
coderowson <runurowson@gmail.com>
2022-04-10 20:58:24 +0600

    to add clone file

---
+++ src/_auth_steps.js
@@ -0,0 +1,15 @@
+// **
+// 1. create a new project in console.google.com
+// 2. npm install firebase
+// 3. create firebase.init.js and import getAuth to export
+// 4. Firebase setting > Authentication > enable Email and password
+// 5.
+
+
+
+//
+// hosting Step
+// 1. npm install -g firebase-tools
+// 2. firebase log in
+// 3. firebase init
+// 4. firebase deploy
---
+++ src/firebase.init.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAAvCihU6ocZbuEE_-hqx89a-guTsn_jco",
+    authDomain: "ema-jhon-simple-1505f.firebaseapp.com",
+    projectId: "ema-jhon-simple-1505f",
+    storageBucket: "ema-jhon-simple-1505f.appspot.com",
+    messagingSenderId: "884964951781",
+    appId: "1:884964951781:web:48f5893fd6a50fac83f60c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+
